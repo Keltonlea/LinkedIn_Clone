cf4cafc5778bcb78c38d6af224d4e4e11bde468e
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-12-31 20:34:41 -0800

    header right w home and supervisor logos, cursor change, styling

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,9 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import linkedinLogo from  "./linkedin.png"
 import "./Header.css"
+import HeaderOption from "./HeaderOption.js"
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 function Header() {
   return (
@@ -17,6 +20,8 @@ function Header() {
       </div>
 
       <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title="Home"/>
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
 
       </div>
     </div>
